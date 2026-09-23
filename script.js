70cf036944620a416438e506f1b6565f895c34a3
Thangapandi <thangapandij@thestackly.com>
2026-09-23 16:23:39 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,264 @@
+// Variables – var
+
+// 1.
+var employeeName = "Thangapandi"
+console.log(employeeName)
+
+// 2.
+var employeeAge = 23
+console.log(employeeAge)
+
+// 3.
+var employeeDesignation
+employeeDesignation = "Frontend Developer"
+console.log(employeeDesignation)
+
+// 4.
+var a = 20
+a = 40
+
+console.log(a)
+
+// 5.
+var b = 35
+var b = 50
+
+console.log(b)
+
+// 6.
+var empName = "Thangapandi"
+var empAge = 23
+var empCity = "Chennai"
+
+console.log(empName)
+console.log(empAge)
+console.log(empCity)
+
+// 7.
+var college = "KV College of Engineering"
+console.log(college)
+
+// 8.
+var favSubject = "Data Science"
+console.log(favSubject)
+
+// 9.
+var c = 10
+c= 25
+c = 45
+c = 65
+
+console.log(c)
+
+// 10.
+var salary = 20000
+salary = 30000
+console.log(salary)
+
+
+// Variables – let
+
+// 11.
+let userName = "Thangapandi"
+console.log(userName)
+
+// 12.
+let usrAge = 23
+console.log(usrAge)
+
+// 13.
+let d
+d = 55
+
+console.log(d)
+
+// 14.
+let e = 100
+e = 200
+
+console.log(e)
+
+// 15.
+let fullName = "Thangapandi"
+let userQualification = "BE"
+let userLocation = "Chennai"
+
+console.log(fullName)
+console.log(userQualification)
+console.log(userLocation)
+
+// 16.
+let course = "JavaScript"
+console.log(course)
+
+// 17.
+let marks = 50
+marks = 80
+
+console.log(marks)
+
+// 18.
+let companyName = "Stackly"
+console.log(companyName )
+
+// 19.
+let experience = 1
+console.log(experience)
+
+// 20.
+let mobileModel = "Oneplus"
+console.log(mobileModel)
+
+
+// Variables – const
+
+// 21.
+const usrname = "Thangapandi"
+console.log(usrname)
+
+// 22.
+const dob = "01/01/2002"
+console.log(dob)
+
+// 23.
+const userCompany = "Stackly"
+console.log(userCompany)
+
+// 24.
+const country = "India"
+console.log(country)
+
+// 25.
+const name = "Thangapandi"
+const age = 24
+const city = "Chennai"
+
+console.log(name)
+console.log(age)
+console.log(city)
+
+// 26.
+const courseName = "JavaScript"
+console.log(courseName)
+
+// 27.
+const collegeName = "KC College of Engineering"
+console.log(collegeName)
+
+// 28.
+const favoriteColor = "Red"
+console.log(favoriteColor)
+
+// 29.
+const employeeId = "EMP001"
+console.log(employeeId)
+
+// 30.
+const officeLocation = "Chennai"
+console.log(officeLocation)
+
+
+// Printing Statements
+
+// 31.
+console.log(100)
+
+// 32.
+console.log("Thangapandi")
+
+// 33.
+console.log(22)
+console.log("BE")
+
+// 34.
+console.log("Welcome to JavaScript")
+
+// 35.
+console.log("Hello World")
+console.log("JavaScript")
+console.log(110)
+console.log("Stackly")
+console.log(80)
+
+// 36.
+var userFullName = "Thangapandi J"
+console.log(userFullName)
+
+// 37.
+var userAge = 24
+console.log(userAge)
+
+// 38.
+var printName = "Thangapandi"
+var printAge = 23
+var printCity = "Chennai"
+
+console.log(printName)
+console.log(printAge)
+console.log(printCity)
+
+
+// Popup Methods
+
+// 39.
+alert("Welcome to JavaScript")
+
+// 40.
+var alertName = "Thangapandi"
+alert(alertName)
+
+// 41.
+var alertUser =  prompt("What is your name?")
+alert(alertUser)
+
+// 42.
+var printUserAge = prompt("What is your age?")
+console.log(printUserAge)
+
+// 43.
+var qualification =  prompt("What is your qualification?")
+alert(qualification)
+
+// 44.
+var printUserCity = prompt("Enter your city?")
+console.log(printUserCity)
+
+// 45.
+let g = "Do you know JavaScript?"
+confirm(g)
+
+// 46.
+let h = "Do you want to continue?"
+confirm(h)
+
+// 47.
+var displayName = prompt("What is your name?")
+var displayqualification = prompt("What is your qualification?")
+
+document.writeln(displayName)
+document.writeln(displayqualification)
+
+
+//document.writeln() + Console Methods
+
+// 48.
+let welcome = "Hello Everyone"
+document.writeln(welcome)
+
+// 49.
+console.log("Thangapandi")
+console.warn("BE")
+console.error("Chennai")
+
+// 50.
+var displayEmpName = prompt("What is your name?")
+var displayEmpAge = prompt("What is your age?")
+var displayEmpQualification = prompt("What is your qualification?")
+
+console.log(displayEmpName)
+console.warn(displayEmpAge)
+console.error(displayEmpQualification)
+
+document.writeln(displayEmpName)
+document.writeln(displayEmpAge)
+document.writeln(displayEmpQualification)
